c11f5e919e7e7dfc46f42bf0ff1d676d05c6c910
Nick Fox-Gieg <nick@fox-gieg.com>
2024-06-06 22:12:56 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -10,6 +10,9 @@ pip install --upgrade torch torchvision torchaudio --index-url https://download.
 cd custom_nodes
 git clone https://github.com/n1ckfg/ComfyUI-Manager
 git clone https://github.com/n1ckfg/comfyui-p5js-node
+git clone https://github.com/n1ckfg/ComfyUI-VideoHelperSuite
+git clone https://github.com/n1ckfg/ComfyUI-AnimateDiff-Evolved
+git clone https://github.com/n1ckfg/ComfyUI-Advanced-ControlNet
 
 cd ..\models\checkpoints
 powershell -Command "Invoke-WebRequest https://civitai.com/api/download/models/128713 -OutFile dreamshaper_8.safetensors"
