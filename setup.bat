ce465bcd5e3d70e58a1e1b57a3c48efbf6453c43
Nick Fox-Gieg <nick@fox-gieg.com>
2025-10-05 00:22:15 -0400

    more depth examples

--- setup.bat
+++ setup.bat
@@ -11,6 +11,7 @@ pip install --upgrade torch torchvision torchaudio --index-url https://download.
 
 cd custom_nodes
 git clone https://github.com/n1ckfg/ComfyUI-Manager
+git clone https://github.com/n1ckfg/ComfyUI_essentials
 git clone https://github.com/n1ckfg/ComfyUI-TiledDiffusion
 git clone https://github.com/n1ckfg/comfyui-p5js-node
 git clone https://github.com/n1ckfg/ComfyUI-VideoHelperSuite
@@ -19,5 +20,6 @@ git clone https://github.com/n1ckfg/ComfyUI-Advanced-ControlNet
 git clone https://github.com/n1ckfg/ComfyUI-Environment-Visualizer
 git clone https://github.com/n1ckfg/Lora-Training-in-Comfy
 git clone https://github.com/n1ckfg/ComfyUI-DepthAnythingV2
+git clone https://github.com/n1ckfg/Comfyui_TTP_Toolset
 
 @pause
