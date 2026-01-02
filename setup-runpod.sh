18d07dc9f6b748012a6f339082d45b8a38faa69b
Nick Fox-Gieg <nick@fox-gieg.com>
2026-01-01 23:52:24 -0600

    Update setup-runpod.sh

--- setup-runpod.sh
+++ setup-runpod.sh
@@ -30,9 +30,9 @@ git clone https://github.com/n1ckfg/ComfyUI-AnimateDiff-Evolved
 git clone https://github.com/n1ckfg/ComfyUI-Crystools
 git clone https://github.com/n1ckfg/ComfyUI-Custom-Scripts
 git clone https://github.com/n1ckfg/ComfyUI-DepthAnythingV2
-git clone https://github.com/n1ckfg/ComfyUI-DepthCrafter-Nodes
+#git clone https://github.com/n1ckfg/ComfyUI-DepthCrafter-Nodes
 git clone https://github.com/n1ckfg/ComfyUI-Environment-Visualizer
-git clone https://github.com/n1ckfg/ComfyUI-FLATTEN
+#git clone https://github.com/n1ckfg/ComfyUI-FLATTEN
 git clone https://github.com/n1ckfg/ComfyUI-GGUF
 git clone https://github.com/n1ckfg/ComfyUI-Impact-Pack
 git clone https://github.com/n1ckfg/comfyui-inpaint-nodes
@@ -45,22 +45,29 @@ git clone https://github.com/n1ckfg/ComfyUI-MultiGPU
 git clone https://github.com/n1ckfg/comfyui-optical-flow
 git clone https://github.com/n1ckfg/comfyui-p5js-node
 git clone https://github.com/n1ckfg/ComfyUI-post-processing-nodes
-git clone https://github.com/n1ckfg/ComfyUI-RAVE
+#git clone https://github.com/n1ckfg/ComfyUI-RAVE
 git clone https://github.com/n1ckfg/ComfyUI-TiledDiffusion
 git clone https://github.com/n1ckfg/comfyui-tooling-nodes
 git clone https://github.com/n1ckfg/ComfyUI-VideoHelperSuite
 git clone https://github.com/n1ckfg/ComfyUI_yanc
-git clone https://github.com/n1ckfg/ComfyUI-WD14-Tagger
+#git clone https://github.com/n1ckfg/ComfyUI-WD14-Tagger
 git clone https://github.com/n1ckfg/ComfyUI_Comfyroll_CustomNodes
 git clone https://github.com/n1ckfg/comfyui_controlnet_aux
 git clone https://github.com/n1ckfg/ComfyUI_Custom_Nodes_AlekPet
 git clone https://github.com/n1ckfg/ComfyUI_essentials
-git clone https://github.com/n1ckfg/comfyui_glslnodes
+#git clone https://github.com/n1ckfg/comfyui_glslnodes
 git clone https://github.com/n1ckfg/ComfyUI_Ib_CustomNodes
 git clone https://github.com/n1ckfg/ComfyUI_IPAdapter_plus
 git clone https://github.com/n1ckfg/ComfyUI_Noise
-git clone https://github.com/n1ckfg/Comfyui_TTP_Toolset
+#git clone https://github.com/n1ckfg/Comfyui_TTP_Toolset
 git clone https://github.com/n1ckfg/execution-inversion-demo-comfyui
 git clone https://github.com/n1ckfg/Image-Captioning-in-ComfyUI
-git clone https://github.com/n1ckfg/Lora-Training-in-Comfy
+#git clone https://github.com/n1ckfg/Lora-Training-in-Comfy
 git clone https://github.com/n1ckfg/was-node-suite-comfyui
+
+cd was-node-suite-comfyui
+pip install -r requirements.txt
+
+cd ../ComfyUI-Impact-Pack
+pip install -r requirements.txt
+
