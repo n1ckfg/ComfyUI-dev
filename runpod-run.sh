05ef1d27ecfd4ea50ba5117a7c869480fc282813
Nick Fox-Gieg <nick@fox-gieg.com>
2026-01-02 14:47:01 -0600

    shell

---
+++ runpod-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "$DIR"
+
+cd /workspace/runpod-slim/ComfyUI
+
+cd custom_nodes/was-node-suite-comfyui
+git clean -fdx
+cd ../..
+
+nohup python3 main.py --listen 0.0.0.0 --port 8188 &
